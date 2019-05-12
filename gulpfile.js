43e8b1d2c5975586d08589f023da768569a29f78
Paul Mospan <pavel.mospan@yahoo.com>
2019-05-12 19:55:41 +0300

    gulpfile.js was not working correctly

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,8 @@ gulp.task('sass', function() { // Создаем таск "sass"
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
     .pipe(gulp.dest('css')) // Выгружаем результата в папку css
   });
-  sass.compiler = require('node-sass');
 
-gulp.task('sass:watcwatch', function() {
+gulp.task('watch', function() {
   gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['sass']); // Наблюдение за sass файлами в папке sass
 });
 
